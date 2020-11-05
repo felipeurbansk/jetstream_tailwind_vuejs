8b9c86030832ef7429d14b77049707407c761694
Felipe Urbanski <felipeurbansk@gmail.com>
2020-11-04 23:11:07 -0300

    Initial commit

---
+++ resources/js/Layouts/AppBase.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen flex flex-col lg:flex-row">
+    <nav class="w-full lg:w-1/5 bg-green-700"></nav>
+    <main class="w-full lg:w-4/5">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
